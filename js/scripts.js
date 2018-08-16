ad96f45039857a3bd7ccd40f55317f1d4f06751d
Regina Nurieva and Victoria Oh <victoria@>
2018-08-16 10:04:52 -0700

    implement and pass first spec. Non numeric character returns false.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,49 @@
+// function convertToRoman(num){
+//   var romans = {
+//     I = 1
+//     V = 5
+//     X = 10
+//     L = 50
+//     C = 100
+//     D = 500
+//     M = 1,000
+//   };
+// }
+
+//Check if string has a number
+function hasNum(string) {
+  var regex = /\d/g;
+  return regex.test(string);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
 
   $("#formOne").submit(function(event) {
     event.preventDefault();
     var userInput = $("#inputBox").val();
     var inputArray = userInput.split(" ");
+    alert(hasNum(userInput));
+    // var outputResult = eachNum(inputArray);
 
-    var outputResult = eachNum(inputArray);
+    // $(".output").text(outputResult);
 
-    $(".output").text(outputResult);
-  })
-})
+  });
+});
